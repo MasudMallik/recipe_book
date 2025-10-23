b3f6f089d7dc0b6a84e63769ef7872a27ee20240
MasudMallik <masudmallik96@gmail.com>
2025-10-23 22:45:10 +0530

    creating home page

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-import random
+from fastapi import FastAPI
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse,RedirectResponse
+from pydantic import BaseModel
+from typing import List,Optional
+
+app=FastAPI()
+app.mount("/static",StaticFiles(directory="static"),name="static")
+templates=Jinja2Templates(directory="templates")
+
+@app.get("/")
+def home():
+    return {"message":"hello world"}
